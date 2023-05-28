9cb498b26dd3768d2f75c96724ad01dceb2523fa
ethankuo <ethankuo0218@gmail.com>
2023-05-28 13:08:50 +0800

    Feature: Initial walkthrough

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    redirectTo: '/walkthrough',
+    pathMatch: 'full',
+  },
+  {
+    path: 'walkthrough',
+    loadChildren: () =>
+      import('./walkthrough/walkthrough.module').then(
+        (m) => m.WalkthroughPageModule
+      ),
+  }
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {
+      // This value is required for server-side rendering to work.
+      initialNavigation: 'enabledBlocking',
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+    }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
