b3e118f3faf30e2366f0949c1076d13403360349
ethankuo218 <ethankuo0218@gmail.com>
2023-06-18 09:30:54 +0800

    Change app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.crowdmeet.app',
   appName: 'crowd-meet-client',
   webDir: 'dist/app/browser',
   bundledWebRuntime: false,
