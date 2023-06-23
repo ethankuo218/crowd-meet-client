259d49547a41ae5bd84c788101688b80c7da161f
Henry Wang <henrywangsime@gmail.com>
2023-06-23 21:01:45 +0800

    chat room check point before optimize

--- src/app/chat/chat-room/chat-room.component.ts
+++ src/app/chat/chat-room/chat-room.component.ts
@@ -1,39 +1,58 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Auth, User, user } from '@angular/fire/auth';
 import {
-  DocumentReference,
   Firestore,
-  addDoc,
+  runTransaction,
   collection,
   collectionData,
   limit,
   orderBy,
-  query
+  query,
+  doc,
+  getDoc,
+  updateDoc
 } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, take } from 'rxjs';
-import { ChatMessage } from '../models/chat.models';
+import { ChatMessage, ReadInfo } from '../models/chat.models';
 
 @Component({
   selector: 'app-chat-room',
   templateUrl: './chat-room.component.html',
   styleUrls: ['./chat-room.component.scss']
 })
-export class ChatRoomComponent implements OnInit {
+export class ChatRoomComponent implements OnInit, OnDestroy {
   constructor(
     private readonly firestore: Firestore,
-    private readonly route: ActivatedRoute
+    private readonly route: ActivatedRoute,
+    private readonly auth: Auth
   ) {}
 
   readonly chatId: string = this.route.snapshot.paramMap.get('id')!;
-  private auth: Auth = inject(Auth);
   user$ = user(this.auth);
   private user: User | null = null;
+  private readInfos: ReadInfo = {};
   messages$!: Observable<ChatMessage[]>;
 
-  ngOnInit() {
+  async ngOnInit() {
     this.messages$ = this.getChatMessages(this.chatId);
-    this.user$.pipe(take(1)).subscribe((user) => (this.user = user));
+    this.user$.pipe(take(1)).subscribe(async (user) => {
+      this.user = user;
+      await this.setReadInfo();
+      await this.updateReadTimeStamp();
+    });
+  }
+
+  ngOnDestroy() {
+    this.updateReadTimeStamp();
+  }
+
+  private async setReadInfo(): Promise<void> {
+    const chatDoc = doc(this.firestore, 'chats', this.chatId);
+    const chatData = (await getDoc(chatDoc)).data();
+    if (chatData) {
+      this.readInfos = chatData['readInfos'];
+    }
   }
 
   public getChatMessages(chatId: string): Observable<ChatMessage[]> {
@@ -46,7 +65,7 @@ export class ChatRoomComponent implements OnInit {
     return collectionData(messagesQuery) as Observable<ChatMessage[]>;
   }
 
-  sendMessage({ target }: Event) {
+  async sendMessage({ target }: Event) {
     const messageText = (target as HTMLInputElement).value;
     if (!messageText) return;
     const message: ChatMessage = {
@@ -55,12 +74,35 @@ export class ChatRoomComponent implements OnInit {
       timestamp: Date.now()
     };
 
+    const chatDoc = doc(this.firestore, 'chats', this.chatId);
     const messagesCollection = collection(
       this.firestore,
       'chats',
       this.chatId,
       'messages'
     );
-    return addDoc(messagesCollection, message);
+
+    // Send the message and update the chat's readInfos in a batch
+    await runTransaction(this.firestore, async (transaction) => {
+      const newMessageRef = doc(messagesCollection);
+      transaction.set(chatDoc, { latestMessage: message }, { merge: true });
+      transaction.set(newMessageRef, message);
+    });
+
+    // Clear the input
+    (target as HTMLInputElement).value = '';
+    await this.updateReadTimeStamp();
+  }
+
+  async updateReadTimeStamp() {
+    if (!this.user) {
+      return;
+    }
+    this.readInfos[this.user.uid].readTimestamp = Date.now();
+
+    const chatDoc = doc(this.firestore, 'chats', this.chatId);
+
+    // Update the chat's readInfos with the new readTimestamp and unreadCount
+    await updateDoc(chatDoc, { readInfos: this.readInfos });
   }
 }
--- src/app/chat/models/chat.models.ts
+++ src/app/chat/models/chat.models.ts
@@ -1,6 +1,39 @@
+// the chat data sturcture is like below:
+// chats (collection)
+// |--- chat1 (document)
+// |    |--- chatId: "chat1"
+// |    |--- type: "private"
+// |    |--- members: ["user1FirebaseUid", "user2FirebaseUid"]
+// |    |--- readInfos: {
+// |           |--- user1FirebaseUid: {
+// |               |--- readTimestamp: 1664430182024
+// |           },
+// |           |--- user2FirebaseUid: {
+// |               |--- readTimestamp: 1664430182024
+// |           }
+// |       }
+// |    |--- latestMessage: {
+// |        |--- messageId: "message1"
+// |        |--- senderId: "user1FirebaseUid"
+// |        |--- content: "Hello"
+// |        |--- timestamp: 1664430182024
+// |    }
+// |    |--- messages (sub-collection)
+// |        |--- message1 (document)
+// |            |--- messageId: "message1"
+// |            |--- senderId: "user1FirebaseUid"
+// |            |--- content: "Hello"
+// |            |--- timestamp: 1664430182024
+
 export interface ChatMessage {
   messageId?: string;
   senderId: string;
   content: string;
   timestamp: number;
 }
+
+interface UserReadInfo {
+  readTimestamp: number;
+}
+
+export type ReadInfo = { [userId: string]: UserReadInfo };
