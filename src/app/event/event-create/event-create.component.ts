dbd4a0c4075187d963c582c3d25ed7c086bf25f6
Ethan Kuo <ethan.kuo@sunbirddcim.com>
2023-07-05 16:31:12 +0800

    Move location search to event create component

--- src/app/event/event-create/event-create.component.ts
+++ src/app/event/event-create/event-create.component.ts
@@ -1,13 +1,22 @@
 import { ImgUploadService } from '../../core/img-upload.service';
 import { EventService } from '../../core/event.service';
 import { ReferenceStateFacade } from '../../core/states/reference-state/reference.state.facade';
-import { Component, OnInit } from '@angular/core';
-import { Validators, FormControl, FormGroup, FormArray } from '@angular/forms';
+import { Component, NgZone, OnInit, ViewChild } from '@angular/core';
+import {
+  Validators,
+  FormControl,
+  FormGroup,
+  FormArray,
+  AbstractControl
+} from '@angular/forms';
 
 import { counterRangeValidator } from '../../components/counter-input/counter-input.component';
 import { take } from 'rxjs';
 import { Router } from '@angular/router';
 import { Category } from 'src/app/core/states/reference-state/reference.model';
+import { GoogleMapsLoaderService } from 'src/app/core/google-maps-loader.service';
+import { Geolocation } from '@capacitor/geolocation';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-page',
@@ -16,6 +25,9 @@ import { Category } from 'src/app/core/states/reference-state/reference.model';
 })
 export class EventCreateComponent implements OnInit {
   eventCoverPictureUrl: string | undefined;
+
+  @ViewChild('searchInput', { read: IonInput })
+  public searchElementRef!: IonInput;
 
   eventForm: FormGroup = new FormGroup({
     title: new FormControl('', [Validators.required]),
@@ -28,32 +40,23 @@ export class EventCreateComponent implements OnInit {
     categories: new FormArray([], [Validators.required])
   });
 
-  validations = {
-    title: [{ type: 'required', message: 'Title is required.' }],
-    description: [{ type: 'required', message: 'Description is required.' }],
-    startTime: [{ type: 'required', message: 'Start time is required.' }],
-    endTime: [{ type: 'required', message: 'End time is required.' }],
-    categories: [{ type: 'required', message: 'Categories is required.' }],
-    maxParticipants: [
-      { type: 'required', message: 'Max participants is required.' },
-      { type: 'rangeError', message: 'Input participants is out of range.' }
-    ],
-    price: [{ type: 'required', message: 'Price is required.' }],
-    locationName: [{ type: 'required', message: 'Location name is required' }],
-    image: [{ type: 'required', message: 'Image is required' }]
-  };
-
   categoryList: Category[] = [];
 
   get categories(): FormArray {
     return <FormArray>this.eventForm.get('categories');
   }
 
+  get location(): AbstractControl {
+    return this.eventForm.get('locationName')!;
+  }
+
   constructor(
     private referenceStateFacade: ReferenceStateFacade,
     private eventService: EventService,
     private router: Router,
-    private imgUploadService: ImgUploadService
+    private imgUploadService: ImgUploadService,
+    private googleMapsLoaderService: GoogleMapsLoaderService,
+    private ngZone: NgZone
   ) {}
 
   ngOnInit(): void {
@@ -68,6 +71,43 @@ export class EventCreateComponent implements OnInit {
           });
         }
       });
+
+    this.googleMapsLoaderService.load().then(async (placesService) => {
+      const coordinates = await Geolocation.getCurrentPosition();
+      const userLocation = {
+        lat: coordinates.coords.latitude,
+        lng: coordinates.coords.longitude
+      };
+      // Create a LatLngBounds object centered around the user's location.
+      const circle = new google.maps.Circle({
+        center: userLocation,
+        radius: 50000
+      }); // 50000 meters = 50 km
+      const bounds = circle.getBounds()!;
+
+      // Set the bounds of the Autocomplete object to the user's location.
+      const options: google.maps.places.AutocompleteOptions = {
+        bounds: bounds
+      };
+
+      const inputElement = await this.searchElementRef.getInputElement();
+      const autocomplete = new google.maps.places.Autocomplete(
+        inputElement,
+        options
+      );
+      autocomplete.addListener('place_changed', () => {
+        this.ngZone.run(() => {
+          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+          console.log(place);
+          // store the data below
+          console.log(place.place_id);
+          console.log(place.geometry?.location?.lat());
+          console.log(place.geometry?.location?.lng());
+          console.log(place.formatted_address);
+          this.location.setValue(place.formatted_address || '');
+        });
+      });
+    });
   }
 
   onSubmit() {
