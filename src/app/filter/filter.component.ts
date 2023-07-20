d5a41dc4ed12710b9fafbef7e68e5685b4e7d2f5
ethankuo218 <ethankuo0218@gmail.com>
2023-07-20 22:21:27 +0800

    Fix time zone format issue

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -135,11 +135,14 @@ export class FilterComponent implements OnInit {
 
   private getFormatTimeString(input?: string): string {
     const date = input ? new Date(input) : new Date();
-    const isoDateString = date.toISOString().split('T')[0];
+    const isoDateString = date.toLocaleDateString().split('/');
     const isMorning = date.toLocaleTimeString().split(' ')[1] === 'AM';
     const isoTimeString = date.toLocaleTimeString().split(' ')[0].split(':');
 
-    return `${isoDateString}T${
+    return `${isoDateString[2]}-${isoDateString[0].padStart(
+      2,
+      '0'
+    )}-${isoDateString[1].padStart(2, '0')}T${
       isMorning
         ? isoTimeString[0].padStart(2, '0')
         : Number(isoTimeString[0]) + 12
