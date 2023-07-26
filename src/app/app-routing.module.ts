5c599ec5845452a03ceb0b5902b6547e547ccb94
ethankuo218 <ethankuo0218@gmail.com>
2023-07-26 21:21:35 +0800

    Remove SSR related file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,27 +5,20 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: '/auth/sign-in',
-    pathMatch: 'full',
+    pathMatch: 'full'
   },
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule)
   },
   {
     path: 'app',
     loadChildren: () =>
-      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
-  },
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule)
+  }
 ];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {
-      // This value is required for server-side rendering to work.
-      initialNavigation: 'enabledBlocking',
-      scrollPositionRestoration: 'enabled',
-      anchorScrolling: 'enabled',
-    }),
-  ],
-  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
