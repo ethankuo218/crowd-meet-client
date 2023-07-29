0633bd740a6d5245779795d22990fe35055374b6
ethankuo218 <ethankuo0218@gmail.com>
2023-07-29 09:55:12 +0800

    Implement reload in joiner list

--- src/app/history/joiner-list/joiner-list.component.ts
+++ src/app/history/joiner-list/joiner-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, from, switchMap } from 'rxjs';
+import { RefresherCustomEvent } from '@ionic/angular';
+import { Subject, firstValueFrom } from 'rxjs';
 import { EventStatus } from 'src/app/core/+states/user-state/user.model';
 import { EventService } from 'src/app/core/event.service';
 import { Participant } from 'src/app/event/models/event.model';
@@ -15,25 +16,38 @@ export class JoinerListComponent implements OnInit {
   private eventService = inject(EventService);
 
   private eventId!: number;
-  joiners$ = from(this.route.params).pipe(
-    switchMap((params): Observable<Participant[]> => {
-      this.eventId = params['id'];
-      return this.eventService.getParticipants(params['id']);
-    })
-  );
+
+  private joinerSubject: Subject<Participant[]> = new Subject();
+
+  joiners$ = this.joinerSubject;
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.eventId = params['id'];
+      this.reload();
     });
   }
 
+  async reload(event?: Event): Promise<void> {
+    const result = await firstValueFrom(
+      this.eventService.getParticipants(this.eventId)
+    );
+    this.joinerSubject.next(result);
+    if (event) {
+      (event as RefresherCustomEvent).target.complete();
+    }
+  }
+
   accept(id: number): void {
-    this.eventService.accept(this.eventId, [id]).subscribe(() => {});
+    this.eventService.accept(this.eventId, [id]).subscribe(() => {
+      this.reload();
+    });
   }
 
   decline(id: number): void {
-    this.eventService.decline(this.eventId, [id]).subscribe();
+    this.eventService.decline(this.eventId, [id]).subscribe(() => {
+      this.reload();
+    });
   }
 
   get eventStatus(): typeof EventStatus {
