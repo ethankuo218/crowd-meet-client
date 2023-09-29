124370b6a21a7cfc401fc65ac9aa886ec6ba6872
ethankuo218 <ethankuo0218@gmail.com>
2023-09-29 16:28:45 +0800

    FIx event patch date time problem

--- src/app/core/formatter.ts
+++ src/app/core/formatter.ts
@@ -1,13 +1,20 @@
+import { DatePipe } from '@angular/common';
+
 export function getFormatTimeString(input?: string): string {
   const date = input ? new Date(input) : new Date();
-  const isoDateString = date.toLocaleDateString().split('/');
-  const isMorning = date.toLocaleTimeString().split(' ')[1] === 'AM';
-  const isoTimeString = date.toLocaleTimeString().split(' ')[0].split(':');
+  const isoDateString = new DatePipe('en-US')
+    .transform(date.toLocaleDateString(), 'MM/dd/yyyy')
+    ?.split('/');
+  const isMorning = date.toLocaleTimeString('en-US').split(' ')[1] === 'AM';
+  const isoTimeString = date
+    .toLocaleTimeString('en-US')
+    .split(' ')[0]
+    .split(':');
 
-  return `${isoDateString[2]}-${isoDateString[0].padStart(
+  return `${isoDateString![2]}-${isoDateString![0].padStart(
     2,
     '0'
-  )}-${isoDateString[1].padStart(2, '0')}T${
+  )}-${isoDateString![1].padStart(2, '0')}T${
     isMorning
       ? isoTimeString[0].padStart(2, '0')
       : Number(isoTimeString[0]) + 12
